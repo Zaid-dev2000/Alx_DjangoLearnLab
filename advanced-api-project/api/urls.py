d2984de1e187f63ce2477eaea2cc47163910f94b
Zaid-dev2000 <elouakhchachizaid@gmail.com>
2024-12-02 22:37:30 +0100

    Added generic views for Book model with CRUD operations

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     
     # Create, Update, and Delete Views
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
