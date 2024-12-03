337f84f3bf38995dadaead44da90102e43634f27
Zaid-dev2000 <elouakhchachizaid@gmail.com>
2024-12-03 22:06:53 +0100

    Added filtering, searching, and ordering functionality for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
 from rest_framework import generics
+from rest_framework import filters
 
 
 class BookListView(ListAPIView):
