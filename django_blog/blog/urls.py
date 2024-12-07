4b067360bfdc36c4f4ccb451a38c78df600ddaad
Zaid-dev2000 <elouakhchachizaid@gmail.com>
2024-12-08 00:17:28 +0100

    Add setup

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,11 +6,11 @@ from .views import (
 from . import views
 
 urlpatterns = [
-     path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='post-list'),  # List all posts
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a single post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
