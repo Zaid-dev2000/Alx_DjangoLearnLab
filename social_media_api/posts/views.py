5af8b9929c4a0d311b07f2b65645d8296b8909df
Zaid-dev2000 <elouakhchachizaid@gmail.com>
2024-12-14 01:03:32 +0100

    Add follow/unfollow functionality and feed endpoint

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -41,7 +41,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         user = request.user
         # Get posts from users the current user is following, ordered by created_at
         followed_users = user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by
+        posts = ["Post.objects.filter(author__in=following_users).order_by"]
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
